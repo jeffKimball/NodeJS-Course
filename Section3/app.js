34363c6aa65b9502e0631764e8b90f58f39cb548
J Kimball <jeffkimballis@gmail.com>
2024-01-11 20:24:06 -0600

    something something about chunks and parsing a body

--- Section3/app.js
+++ Section3/app.js
@@ -1,7 +1,42 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res) =>{
-    console.log(req.url, req.method, req.headers)
+    const url = req.url
+    const method = req.method
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html')
+            res.write('<html>')
+            res.write('<head><title>Enter Message</title></head>')
+            res.write('<body><form action="/message" method="POST"><input name="msg"></input><button>Send</button type=""submit></form></body>')
+            res.write('</html>')
+        return  res.end()
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = []
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody)
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
+        
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+
+    res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Hello from my JS Server</h1></body>')
+        res.write('</html>')
+    res.end()
+   
 })
 
 server.listen(3000)
@@ -33,19 +68,4 @@ server.listen(3000)
 
 
 
-
-// const http = require('http')
-
-// const server = http.createServer((req, res) => {
-//     console.log(req.url, req.method, req.headers)
-//     // process.exit()
-//     res.setHeader('Content-Type', 'text/html')
-//     res.write('<html>')
-//     res.write('<head><title>My First Page</title></head>')
-//     res.write('<body><h1>Hello from my JS Server</h1></body>')
-//     res.write('</html>')
-//     res.end()
-// })
-
-// server.listen(3000)
 
