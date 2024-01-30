ed2a8ba33b68830182fb3b544a698a65ba54dc81
J Kimball <jeffkimballis@gmail.com>
2024-01-29 18:21:27 -0600

    WIP

--- Section3/app.js
+++ Section3/app.js
@@ -1,20 +1,18 @@
-const express = require('express')
+const http = require('http')
 
-const app = express()
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Add product page</h1>')
+const server = http.createServer((req, res)=>{
+    console.log(req.url, req.method, req.headers)
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello From My Node JS Server</h1></body>')
+    res.write('</head>')
+    res.end()
 })
 
-app.use('/', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Hello from express</h1>')
-})
-
-app.listen(3000)
-
-
+server.listen(3000)
 
 
 
